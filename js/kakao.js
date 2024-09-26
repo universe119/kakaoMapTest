e340c84c504a33b25f7aae7ddb9d8ae261f7206f
universe119 <ruddh119@gmail.com>
2024-09-26 17:02:40 +0900

    교통량정보를 토글기능으로 변경

--- js/kakao.js
+++ js/kakao.js
@@ -9,14 +9,36 @@ const map = new kakao.maps.Map(mapContainer, mapOption);
 const marker = new kakao.maps.Marker({ position: mapOption.center });
 marker.setMap(map);
 
-// 맵 위에 올릴 타입 컨트롤 컨트롤 인스턴스 생성
 const mapTypeControl = new kakao.maps.MapTypeControl();
 
-//맵 위에 올릴 줌 컨트롤 인스턴스 생성
 const zoomControl = new kakao.maps.ZoomControl();
 
-// 타입컨트롤러의 위치값 지정하는 함수
 map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 
-// 줌 컨트롤러의 위치값 지정하는 함수
 map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+// const [btnOn, btnOff] = document.querySelectorAll("nav button");
+
+// // 지도인스턴스에 교통량정보 레이어 추가
+// btnOn.addEventListener("click", () =>
+// 	map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+// );
+
+// // 아래 코드는 위에서 추가한 교통정보 지도타입을 제거합니다
+// btnOff.addEventListener("click", () =>
+// 	map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+// );
+
+const btnToggle = document.querySelector(".trafficToggle");
+
+btnToggle.addEventListener("click", (e) => {
+	e.target.classList.toggle("on");
+
+	if (e.target.classList.contains("on")) {
+		map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+		e.target.innerText = "Traffic OFF";
+	} else {
+		map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+		e.target.innerText = "Traffic ON";
+	}
+});
