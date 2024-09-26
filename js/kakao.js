9ac7338a4f243dc04d50382b9ccb71aca66f0e42
universe119 <ruddh119@gmail.com>
2024-09-26 16:06:18 +0900

    위성지도 타입 변경 컨트롤러 추가

--- js/kakao.js
+++ js/kakao.js
@@ -1,23 +1,19 @@
-// new kakao.maps.LatLng(위도, 경도) : 지도와 마커를 출력할때 필요한 위치 인스턴스 반환
-// new kakao.maps.Map(DOM, option) : 지도 인스턴스 반환
-// new kakao.maps.Market({position: 위치 인스턴스}) : 특정 위치에 생성되는 마커인스턴스 반환
-
-// 마커인스턴스.setMap(지도 인스턴스) : 기존 지도에 마커를 세팅해주는 함수
-
-//frame El
 const mapContainer = document.querySelector("#map");
 
-//map option(position instance, level)
 const mapOption = {
 	center: new kakao.maps.LatLng(37.58206195368533, 127.00013868670247),
 	level: 2,
 };
 
-//map instance
 const map = new kakao.maps.Map(mapContainer, mapOption);
-
-//marker instance
 const marker = new kakao.maps.Marker({ position: mapOption.center });
-
-//binding marker
 marker.setMap(map);
+
+// 맵위에 올릴 컨트롤 인스턴스 생성
+const mapTypeControl = new kakao.maps.MapTypeControl();
+const zoomControl = new kakao.maps.ZoomControl();
+
+// 타입컨트롤러의 위치값 지정하는 함수
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
