4c7d8d8ae4780c3b8552542188bc9e530494fce6
universe119 <ruddh119@gmail.com>
2024-09-27 10:09:58 +0900

    마커올리기

--- js/kakao.js
+++ js/kakao.js
@@ -1,16 +1,11 @@
 const mapContainer = document.querySelector("#map");
 const btnToggle = document.querySelector(".trafficToggle");
+const viewToggle = document.querySelector(".viewToggle");
 
 const mapOption = {
 	center: new kakao.maps.LatLng(37.58206195368533, 127.00013868670247),
 	level: 2,
 };
-
-// 스크립트가 처음 로드된 시점에 mapOption 안에 포함되어 있는 위치값을 기준으로 지도 인스턴스가 생성되고 끝
-// 이슈사항: 지도 인스턴스가 처음 생성된 시점에서 위치값이 고정되어 있기 때문에 브라우저 리사이즈시 위치 중앙이 틀어짐
-// 해결방법: 브라우저 리사이즈 할때마다 지도 인스턴스를 생성하면됨.
-
-// 브라우저 리사이즈 될때마다 map 변수에 변경된 값을 재반영해야 되므로 let방식으로 변수 선언
 let map = new kakao.maps.Map(mapContainer, mapOption);
 let marker = new kakao.maps.Marker({ position: mapOption.center });
 const mapTypeControl = new kakao.maps.MapTypeControl();
@@ -22,30 +17,15 @@ marker.setMap(map);
 
 window.addEventListener("resize", () => {
 	mapContainer.innerHTML = "";
-	// 기존 map, marker 변수에 변경된 인스턴스 정보값을 덮어쓰기 처리
 	map = new kakao.maps.Map(mapContainer, mapOption);
 	marker.setMap(map);
 
-	//리사이즈 될때마다 컨트롤 패널 다시 추가
 	map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
 	map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
-	// 리사이즈시 강제 토글버튼 초기화
 	btnToggle.classList.remove("on");
 	btnToggle.innerText = "Traffic ON";
 });
-
-// const [btnOn, btnOff] = document.querySelectorAll("nav button");
-
-// // 지도인스턴스에 교통량정보 레이어 추가
-// btnOn.addEventListener("click", () =>
-// 	map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
-// );
-
-// // 아래 코드는 위에서 추가한 교통정보 지도타입을 제거합니다
-// btnOff.addEventListener("click", () =>
-// 	map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
-// );
 
 btnToggle.addEventListener("click", (e) => {
 	e.target.classList.toggle("on");
@@ -57,4 +37,31 @@ btnToggle.addEventListener("click", (e) => {
 		map.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 		e.target.innerText = "Traffic ON";
 	}
+});
+
+//roadview setting
+const viewContainer = document.querySelector("#view"); //로드뷰를 표시할 div
+const view = new kakao.maps.Roadview(viewContainer); //로드뷰 객체
+const viewClient = new kakao.maps.RoadviewClient(); //좌표로부터 로드뷰 파노ID를 가져올 로드뷰 helper객체
+
+// 특정 위치의 좌표와 가까운 로드뷰의 panoId를 추출하여 로드뷰를 띄운다.
+viewClient.getNearestPanoId(mapOption.center, 50, (panoId) => {
+	view.setPanoId(panoId, mapOption.center); //panoId와 중심좌표를 통해 로드뷰 실행
+});
+
+// 로드뷰에 마커 올리기
+kakao.maps.event.addListener(view, "init", () => {
+	// 로드뷰에 올릴 마커를 생성합니다.
+	const rMarker = new kakao.maps.Marker({
+		position: mapOption.center,
+		map: view, //view로 설정하면 로드뷰에 올라갑니다.
+	});
+
+	// 로드뷰에 올릴 장소명 인포윈도우를 생성합니다.
+	const rLabel = new kakao.maps.InfoWindow({
+		position: mapOption.center,
+		content: "서교주담",
+	});
+
+	rLabel.open(view, rMarker);
 });
